ddd92e066f79131ce76909bc177ec856a840dea0
Start Draft <start.draft@gmail.com>
2022-02-03 13:21:01 +0300

    Committed Hello1

---
+++ Hello1.java
@@ -0,0 +1,10 @@
+import Hello1
+
+class Hello1{
+    Hello var1 = new Hello("Nice to see you", "Anna");
+
+    public static void main(String[] args) {
+        var1.getGreeting();
+        var1.getName();
+
+}
