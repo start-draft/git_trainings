0db51a7bf27b5498b78a75b6edd7d6c356dc65c0
Start Draft <start.draft@gmail.com>
2022-02-06 13:26:24 +0300

    First attempt to write commit message via vim.

--- Hello3.java
+++ Hello3.java
@@ -1,3 +1,6 @@
 class Hello3 {
-    String value01 = "It's the second commit for Hello3.java";
+    String value01 = "It's the second commit for Hello3.java and we are happy!";
+
+    String value02 = "Hi!";
+    String value03 = "It's the third commit for Hello3.java";
 }
