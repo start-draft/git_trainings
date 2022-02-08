5b36eb2bf994ab40f3750a869ba725579051a0d6
Start Draft <start.draft@gmail.com>
2022-02-08 10:47:15 +0300

    Committed Hello4.java with corrections

--- Hello3.java
+++ Hello3.java
@@ -2,5 +2,7 @@ class Hello3 {
     String value01 = "It's the second commit for Hello3.java and we are happy!";
 
     String value02 = "Hi!";
-    String value03 = "It's the third commit for Hello3.java";
+    String value03 = "It's the third commit for this file";
+
+    String value04 = "It's the forth commit for Hello3.java";
 }
