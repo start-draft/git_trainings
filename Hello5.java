3056b1e5354043d8900861699fdfc4059c09db7e
Start Draft <start.draft@gmail.com>
2022-02-11 18:43:31 +0300

    -Added Hello5.java.

---
+++ Hello5.java
@@ -0,0 +1,7 @@
+class Hello5 {
+    private final String value01 = "It's first String value at Hello5.java";
+
+    private final String value02 = "It's second String value at Hello5.java";
+
+    private final String value03 = "It's third String value at Hello5.java";
+}
