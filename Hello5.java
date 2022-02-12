14a71f35d9e78760bcaaea6a9a2475a837872c5d
Start Draft <start.draft@gmail.com>
2022-02-12 09:54:37 +0300

    - First commit for `testing` branch

--- Hello5.java
+++ Hello5.java
@@ -4,4 +4,6 @@ class Hello5 {
     private final String value02 = "It's second String value at Hello5.java";
 
     private final String value03 = "It's third String value at Hello5.java";
+
+    private final String value04 = "It's forth String value at Hello5.java for 'testing' branch";
 }
